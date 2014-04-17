6441cd714485502d9787451c22dfd6303833933e
TweetPressFr <tweetpressfr@gmail.com>
2014-04-17 18:11:04 +0200

    update version number

--- jm-twitter-cards.php
+++ jm-twitter-cards.php
@@ -47,7 +47,7 @@ or die('What we\'re dealing with here is a total lack of respect for the law !')
 
 
 //Constantly constant
-define( 'JM_TC_VERSION', '5.1.8' );
+define( 'JM_TC_VERSION', '5.1.9' );
 define( 'JM_TC_DIR', plugin_dir_path( __FILE__ )  );
 define( 'JM_TC_INC_DIR', trailingslashit(JM_TC_DIR . 'inc') );
 define( 'JM_TC_ADMIN_DIR', trailingslashit(JM_TC_DIR . 'inc/admin') );
