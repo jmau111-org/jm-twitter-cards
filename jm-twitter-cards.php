c3fff7d08648210535ca761d0ff40d8f0d972f41
TweetPressFr <tweetpressfr@gmail.com>
2014-06-07 09:41:02 +0200

    update version num

--- jm-twitter-cards.php
+++ jm-twitter-cards.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.tweetpress.fr
 Description: Meant to help users to implement and customize Twitter Cards easily
 Author: Julien Maury
 Author URI: http://www.tweetpress.fr
-Version: 5.2.4
+Version: 5.2.5
 License: GPL2++
 
 JM Twitter Cards Plugin
@@ -46,7 +46,7 @@ or die('What we\'re dealing with here is a total lack of respect for the law !')
 
 
 //Constantly constant
-define( 'JM_TC_VERSION', '5.2.4' );
+define( 'JM_TC_VERSION', '5.2.5' );
 define( 'JM_TC_DIR', plugin_dir_path( __FILE__ )  );
 define( 'JM_TC_INC_DIR', trailingslashit(JM_TC_DIR . 'inc') );
 define( 'JM_TC_ADMIN_DIR', trailingslashit(JM_TC_DIR . 'inc/admin') );
