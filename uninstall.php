7b19cfc4676a3002c52bdf10d91d0e2a1a5d05a1
TweetPressFr <tweetpressfr@gmail.com>
2014-06-12 01:08:00 +0200

    remove multisite loop in uninstall.php
    it seems that in multisite looping through blogs to delete options on
    each blog does not scale.

--- uninstall.php
+++ uninstall.php
@@ -4,62 +4,40 @@
 if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
 	exit();
 
-
-function jm_tc_on_delete() {
 	
-	delete_option( 'jm_tc'); 
+delete_option( 'jm_tc'); 
+delete_site_option( 'jm_tc_network'); 
 
-	/**
-	 * Delete postmeta from option table
-	 *
-	 */
-	$keys = array(
-				'twitterCardType', 
-				'cardImage',
-				'cardPlayer',
-				'cardImageWidth',
-				'cardImageHeight',
-				'cardPlayerWidth',
-				'cardPlayerHeight',
-				'cardPlayerStream',
-				'cardData1',
-				'cardLabel1',
-				'cardData2',
-				'cardLabel2',
-				'cardImgSize',
-			);
-			
-			
-	foreach($keys as $key)	{
-	global $wpdb;
-		$wpdb->query( 
-			$wpdb->prepare( 
-				"
-				 DELETE FROM $wpdb->postmeta
-				 WHERE meta_key = %s
-				",
-				$key
-				)
+/**
+ * Delete postmeta from option table
+ *
+ */
+$keys = array(
+			'twitterCardType', 
+			'cardImage',
+			'cardPlayer',
+			'cardImageWidth',
+			'cardImageHeight',
+			'cardPlayerWidth',
+			'cardPlayerHeight',
+			'cardPlayerStream',
+			'cardData1',
+			'cardLabel1',
+			'cardData2',
+			'cardLabel2',
+			'cardImgSize',
 		);
-	}
-}
-
-if( !is_multisite() ) {
-
-	jm_tc_delete();
-
-} else {
-
-	delete_site_option( 'jm_tc_network'); 
-	
-    // For regular options.
-    global $wpdb;
-    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-    foreach ( $blog_ids as $blog_id ) 
-    {
-        switch_to_blog( $blog_id );
-        jm_tc_delete();  
-		restore_current_blog();
-    }
-	
+		
+		
+foreach($keys as $key)	{
+global $wpdb;
+	$wpdb->query( 
+		$wpdb->prepare( 
+			"
+			 DELETE FROM $wpdb->postmeta
+			 WHERE meta_key = %s
+			",
+			$key
+			)
+	);
 }
