3f5177d28f37a98c9b59dfbd4e23bf7c53d36a61
TweetPressFr <tweetpressfr@gmail.com>
2014-06-22 18:00:16 +0200

    update version const

--- jm-twitter-cards.php
+++ jm-twitter-cards.php
@@ -47,7 +47,7 @@ or die('What we\'re dealing with here is a total lack of respect for the law !')
 
 
 //Constantly constant
-define( 'JM_TC_VERSION', '5.2.6' );
+define( 'JM_TC_VERSION', '5.2.7' );
 define( 'JM_TC_DIR', plugin_dir_path( __FILE__ )  );
 define( 'JM_TC_INC_DIR', trailingslashit(JM_TC_DIR . 'inc') );
 define( 'JM_TC_ADMIN_DIR', trailingslashit(JM_TC_DIR . 'inc/admin') );
