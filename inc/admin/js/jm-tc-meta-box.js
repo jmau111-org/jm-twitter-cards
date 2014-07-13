19ba71a38fb30d1a055eb71e0aa93c8c2b3acb7a
TweetPressFr <tweetpressfr@gmail.com>
2014-07-13 14:57:09 +0200

    remove code in js

--- inc/admin/js/jm-tc-meta-box.js
+++ inc/admin/js/jm-tc-meta-box.js
@@ -1,27 +1,7 @@
 // DOM ready
 jQuery(document).ready(function($) {
 
-		
-		busy = $.ajax({
-			url: ajaxurl,
-			type: 'POST',
-			data: form.serialize(),
-			success: function( response ) {
-				
-				if( response == 'JM_TC_Options' ) {
-					
-			
-					console.log('ok');
-				}
-				
-
-			}
-		});
-			
-		
-	}
 	
-	return false;
 	
 });
 		
