8031b271fa6c6406de028d5688e45d051c7e5e78
Julien Maury <tweetpressfr@gmail.com>
2014-07-18 19:06:51 +0200

    one line

--- jm-twitter-cards.php
+++ jm-twitter-cards.php
@@ -24,7 +24,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 *    Sources: 
 * - https://dev.twitter.com/docs/cards
 * - https://dev.twitter.com/docs/cards/getting-started#open-graph
