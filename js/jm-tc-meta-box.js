995a766d929d1883428bba1d82d50bef4dee5ce7
tweetpressfr <tweetpressfr@gmail.com>
2015-07-25 23:18:28 +0200

    fix js

--- js/jm-tc-meta-box.js
+++ js/jm-tc-meta-box.js
@@ -6,30 +6,6 @@ jQuery(document).ready(function ($) {
      */
 
     //hide
-    function hideProduct() {
-        $('.cmb_id_product_title').hide();
-        $('.cmb_id_cardData1').hide();
-        $('.cmb_id_cardLabel1').hide();
-        $('.cmb_id_cardData2').hide();
-        $('.cmb_id_cardLabel2').hide();
-    }
-
-    function showProduct() {
-        $('.cmb_id_product_title').show(500);
-        $('.cmb_id_cardData1').show(500);
-        $('.cmb_id_cardLabel1').show(500);
-        $('.cmb_id_cardData2').show(500);
-        $('.cmb_id_cardLabel2').show(500);
-    }
-
-    function hideGallery() {
-        $('.cmb_id_gallery_title').hide();
-    }
-
-    function showGallery() {
-        $('.cmb_id_gallery_title').show(500);
-    }
-
     function hidePlayer() {
         $('.cmb_id_player_title').hide();
         $('.cmb_id_cardPlayer').hide();
@@ -58,53 +34,29 @@ jQuery(document).ready(function ($) {
         $('.cmb_id_cardImage').hide();
     }
 
-    hideProduct();
     hidePlayer();
-    hideGallery();
 
     //see
     $('#twitterCardType').on('change', function () {
 
         if (this.value == 'summary') {
-            hideProduct();
             hidePlayer();
-            hideGallery();
             showBoxImg();
             //console.log('summary');
         } else if (this.value == 'summary_large_image') {
-            hideProduct();
             hidePlayer();
-            hideGallery();
             showBoxImg();
             //console.log('summary large image');
         } else if (this.value == 'photo') {
-            hideProduct();
             hidePlayer();
-            hideGallery();
             showBoxImg();
             //console.log('photo');
-        } else if (this.value == 'product') {
-            hideProduct();
-            hidePlayer();
-            showProduct();
-            showBoxImg();
-            //console.log('product');
         } else if (this.value == 'player') {
-            hideGallery();
-            hideProduct();
             showPlayer();
             showBoxImg();
             //console.log('player');
-        } else if (this.value == 'gallery') {
-            hideProduct();
-            hidePlayer();
-            showGallery();
-            hideBoxImg();
-            //console.log('gallery');
         } else if (this.value == 'application') {
-            hideProduct();
             hidePlayer();
-            hideGallery();
             hideBoxImg();
             //console.log('application');
         } else {
