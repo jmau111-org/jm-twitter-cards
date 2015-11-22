16a24a12dcb0018b29641d1aa40fd10f653acf04
tweetpressfr <jmaury@beapi.fr>
2015-11-22 15:29:36 +0100

    missing uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,44 @@
+<?php
+
+// If cheating exit
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+
+delete_option( 'jm_tc' );
+
+
+/**
+ * Delete postmeta from option table
+ *
+ */
+$keys = array(
+	'twitterCardType',
+	'cardImage',
+	'cardPlayer',
+	'cardImageWidth',
+	'cardImageHeight',
+	'cardPlayerWidth',
+	'cardPlayerHeight',
+	'cardPlayerStream',
+	'cardPlayerCodec',
+	'cardData1',
+	'cardLabel1',
+	'cardData2',
+	'cardLabel2'
+);
+
+
+foreach ( $keys as $key ) {
+	global $wpdb;
+	$wpdb->query(
+		$wpdb->prepare(
+			"
+			 DELETE FROM $wpdb->postmeta
+			 WHERE meta_key = %s
+			",
+			$key
+		)
+	);
+}
