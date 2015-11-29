e1f7d65846f041fdd299bcd5e401d45df407c2c1
tweetpressfr <jmaury@beapi.fr>
2015-11-29 15:20:32 +0100

    video preview

--- views/preview.php
+++ views/preview.php
@@ -20,6 +20,7 @@ $desc   = reset( $this->options->description() );
 $image  = reset( $this->options->image() );
 $domain = $_SERVER['HTTP_HOST'];
 $avatar = get_avatar_url( get_avatar( null, 36 ) );
+$style  = 'summary' === $type ? 'style="background-image:url( ' . esc_url( $image ) . ');"' : '';
 
 $preview .= '<div class="global-' . esc_attr( $type ) . ' tc-preview">';
 $preview .= '<div class="tc-author u-cf">';
@@ -30,8 +31,8 @@ $preview .= '</div>';
 $preview .= '<div class="tc-text u-pullLeft"><p>' . esc_html__( 'The card for your website will look a little something like this!', 'jm-tc' ) . '</p> </div>';
 
 $card = '<div class="tc-summary tc-container">';
-$card .= '<div class="tc-' . esc_attr( $type ) . '-img-container">';
-$card .= '<img class="tc-img ' . esc_attr( $type ) . '-img" src="' . esc_url( $image ) . '" alt="">';
+$card .= '<div class="tc-' . esc_attr( $type ) . '-img-container" ' . $style . '>';
+$card .= 'player' ===  $type ? '<video  class="tc-img" style="-webkit-user-drag: none;" controls poster="' . esc_url( $image ) . '"></video>' : '<img class="tc-img ' . esc_attr( $type ) . '-img" src="' . esc_url( $image ) . '" alt="">';
 $card .= '</div>';
 $card .= '<div class="' . esc_attr( $type ) . '-content"><p class="' . esc_attr( $type ) . '-title tc-title">' . esc_html( $title ) . '</p>
           <p class="' . esc_attr( $type ) . '-desc tc-desc">' . esc_html( $desc ) . '</p>
