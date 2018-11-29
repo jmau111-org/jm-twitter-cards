ae45dfb03d08ac7911512fa514698637ae95a303
Julien Maury <julienmaury111@gmail.com>
2018-11-30 00:27:31 +0100

    fix warning

--- admin/Meta.php
+++ admin/Meta.php
@@ -34,8 +34,12 @@ class Meta {
 
 	protected function add_critical_settings( $cpt ) {
 		$cpt_object = get_post_type_object( $cpt );
-		add_post_type_support( $cpt, 'custom-fields' );// needed
-		$cpt_object->show_in_rest = true; // we only fetch public cpt in Utils::get_post_types()
+
+		if ( ! empty( $cpt_object ) ) {
+			add_post_type_support( $cpt, 'custom-fields' );// needed
+			$cpt_object->show_in_rest = true; // we only fetch public cpt in Utils::get_post_types()
+		}
+
 	}
 
 	/**
